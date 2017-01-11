8771e41da7da263b532ef9b078daed630fc07983
Jairo A. Román Alvia <lex-roman@hotmail.com>
2017-01-10 23:12:59 -0500

    MostrarMensaje
    Mostrar mensaje por consola en Java, 3 formas diferentes con algunas
    anotaciones de variaciones que se pueden hacer al mostrar un codigo por
    la consola.

---
+++ (ejercicio1)CursoC01.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cursoc01;
+
+/**
+ *Programa: 01
+ * Descipcion: Mostar mensaje "Bienvenido a Java"
+ * @author Jairo Roman Alvia
+ * Fecha: Diciembre 2, 2014
+ * Modificado por: Jairo Roman Alvia
+ * Cambio: Muestra "Welcome to Java Programing", "is easy", "But need practise"
+ */
+public class CursoC01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // print=mostar  l=line    n=new
+    System.out.println("Welcome to Java Programing");
+    System.out.print("Is easy \n");// (\n)is equal ln(lineNew)
+    System.out.print("\t But need practise");
+    }
+    
+}
